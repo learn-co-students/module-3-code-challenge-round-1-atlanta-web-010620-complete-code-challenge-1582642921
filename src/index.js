b4672ed5c127dc9f68a5f443c3773cdce9430148
Dino <dinokunovac@gmail.com>
2020-02-25 11:08:22 -0500

    maybe next time

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
 
-  let imageId = 1 //Enter the id from the fetched image here
+  let imageId = 4638
 
   const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
 
@@ -9,4 +9,31 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const commentsURL = `https://randopic.herokuapp.com/comments/`
 
+  fetchImage(imageURL);
+
 })
+
+function fetchImage(imageURL) {
+  fetch(imageURL)
+  .then(res => res.json())
+  .then((image) => {
+    return displayImage(image);
+  });
+}
+
+function displayImage(image) {
+  const mainContainer = document.querySelector('.container')
+  const divImage = document.createElement('div')
+  divImage.querySelector('#image_content')
+  const img = document.createElement('img')
+  img.querySelector('#image')
+  img.dataset.imageId = image.id
+  img.innerText = image.url
+  mainContainer.append
+  mainContainer.appendChild(divImage)
+  divImage.appendChild(img)
+  console.log(img)
+
+  //okay I'm throwing in the towel
+
+}
