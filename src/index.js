fee95847e27ca7dd7955144af9578ebcdea6d19a
Jacy Leech <jacy.atlanta@gmail.com>
2020-02-27 11:25:44 -0500

    revised

--- src/index.js
+++ src/index.js
@@ -6,90 +6,56 @@ const likeURL = `https://randopic.herokuapp.com/likes/`
 
 const commentsURL = `https://randopic.herokuapp.com/comments/`
 
-
-document.addEventListener('DOMContentLoaded', (e) => {
+document.addEventListener('DOMContentLoaded', () => {
   console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
 
-
+getData()
   
-  getTheData()
-
 })
 
-function getTheData()
+function getData() {
 fetch(imageURL)
-.then (resp => resp.json())
-.then (resp => getImage(resp))
-
-function getImage(object){
-  let image = document.querySelector('#image')
-  image.src = object.url
-
-  let imgName = document.querySelector('#name')
-  imgName.name = object.name
-
-  let likes = document.querySelector('span')
-  likes.dataset.id = object.id
-  likes.innerText = object.like_count
-
-  let likeButton = document.querySelector('#like_button')
-  likeButton.dataset.id = object.id
-
-  let likeInput = document.querySelector('#comment_input')
-  likeInput.dataset.id = object.id
-
-  let submitButton = document.querySelector('#submit')
-
-  submitButton.addEventListener('click', function(e){
-    let li = document.createElement('li')
-
-    let ul = document.querySelector('#comments')
-    
-    li.innerText = document.querySelector('#comment_input').value
-    ul.appendChild(li)
-    let commentData = document.querySelector('#comment_input').value
-
-    let comment = {
-      image_id: `${imageId}`, 
-      content: commentData}
-    addChange(comment)
-    // debugger;
-  })
-
-  likeButton.addEventListener('click', function(e){
-    
-      let span = document.querySelector('span');
-      let addLikes = parseInt(document.querySelector('span').innerText)
-        addLikes++
-        span.innerText = addLIKES
-      })
-    }
-
-    function frontEnd(newLikes){
-      fetch('likeURL', {
-        method: "POST",
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(newLikes),
-      })
-      .then((response) => response.json())
-      .then((newLikes) => {
-      console.log('Success:', newLikes)
-      })
-
-    function backEnd(comment){
-      fetch('commentsURL', {
-        method: "POST",
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(comment)
-      })
-      .then(response => response.json())
-      .then((comment) => {
-        console.log('Success:', data)
-    })
+.then(resp => resp.json()) 
+.then(resp => renderInfo(resp))
 }
+
+function renderInfo(pic) {
+  let image = document.querySelector('#image')
+  image.src = pic.url
+
+  let name = document.querySelector('#name')
+  name.innerText = pic.name
+  getLikes()
+}
+
+function getLikesFrontEnd() {
+  
+  let likes = document.querySelector('#likes')
+  let likeButton = document.querySelector('#like_button')
+  likeButton.addEventListener('click',function(e) {
+    let addLikes = parseInt(likes.innerText)
+      addLikes = 0
+      addLikes++
+      likes.innerText = addLikes
+      getLikesBackEnd()
+})
+}
+
+
+function getLikesBackEnd(data) {
+  fetch(imageURL), {
+  method: 'POST',
+  headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify(data),
+}
+.then((response) => response.json())
+.then((data) => {
+  console.log('Success:', data);
+})
+// })
+}
+// I got stuck right in here trying to get this to work. I wasn't able to look at the MDN docs
+// to figure out what i needed to do.
